9aeb481810efb317d3d0305e015e5a8fee3ae574
pfoof <piotr@pabis.eu>
2020-07-26 16:45:41 +0200

    Welcome layout that leads to RegisterActivity

--- app/src/main/java/com/furfel/yarsa_frontend/YARSAFrontend.kt
+++ app/src/main/java/com/furfel/yarsa_frontend/YARSAFrontend.kt
@@ -1,5 +1,6 @@
 package com.furfel.yarsa_frontend
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -7,17 +8,26 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.furfel.yarsa_frontend.register.RegisterActivity
+import kotlinx.android.synthetic.main.welcome_layout.*
 
 class YARSAFrontend : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.view_profile)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = title
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        setContentView(R.layout.welcome_layout)
+
+        signUp.setOnClickListener {
+            if(it.isEnabled) {
+                val intent = Intent(this, RegisterActivity.javaClass)
+                startActivity(intent)
+            }
+        }
+
+        signIn.setOnClickListener {
+            if(it.isEnabled) {
+
+            }
         }
     }
 }
